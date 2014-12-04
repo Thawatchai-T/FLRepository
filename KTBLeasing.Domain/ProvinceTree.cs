3aaecff164ad1e4a8de2ff93fc5011991627b20c
Thawatchai-t <thawatchai.t@carpass.co.th>
2014-12-04 11:35:52 +0700

    add domain ProvinceTree
    Signed-off-by: Thawatchai-t <thawatchai.t@carpass.co.th>

---
+++ KTBLeasing.Domain/ProvinceTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KTBLeasing.Domain
+{
+    public class ProvinceTree
+    {
+        public virtual decimal Id { get; set; }
+        public virtual decimal ParentId { get; set; }
+        public virtual decimal Levels { get; set; }
+        public virtual string Name { get; set; }
+        public virtual short IsLeaf { get; set; }
+        
+           
+        #region NHibernate Composite Key Requirements
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            var t = obj as ProvinceTree;
+            if (t == null) return false;
+            if (Id == t.Id
+                && ParentId == t.ParentId)
+                return true;
+
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            int hash = GetType().GetHashCode();
+            hash = (hash * 397) ^ Id.GetHashCode();
+            hash = (hash * 397) ^ ParentId.GetHashCode();
+
+            return hash;
+        }
+        #endregion
+        
+    }
+}
