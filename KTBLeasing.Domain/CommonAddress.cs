df27ca6a2751c89ef2c91e4f50f80588f2fa3621
Thawatchai-t <thawatchai.t@carpass.co.th>
2014-12-11 08:47:24 +0700

    Add Constructor support query in sql.
    Signed-off-by: Thawatchai-t <thawatchai.t@carpass.co.th>

--- KTBLeasing.Domain/CommonAddress.cs
+++ KTBLeasing.Domain/CommonAddress.cs
@@ -6,6 +6,23 @@ using System.Collections.Generic;
 namespace KTBLeasing.FrontLeasing.Domain {
     
     public class CommonAddress {
+		
+		/** [20151211] Thawatchai.T 
+		*   -Constructor to support Transformers.AliasToBeanConstructor.
+		*   -Remark Req parameter same in sql query. 
+		*/
+        public CommonAddress(decimal ID, decimal PARENT_ID, decimal LEVELS, string NAME, decimal ISLEAF)
+        {
+            this.Id = Convert.ToInt32(ID);
+            this.IsLeaf = Convert.ToBoolean(ISLEAF);
+            this.Levels = Convert.ToInt32(LEVELS);
+            this.Name = NAME;
+            this.Parent_Id = Convert.ToInt32(PARENT_ID);
+        }
+
+        public CommonAddress()
+        {
+        }
         public virtual long Id { get; set; }
         public virtual int Parent_Id { get; set; }
         public virtual int Levels { get; set; }
