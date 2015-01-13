704ad8073220d922203415f018b7eb13c0655a8f
Thawatchai-t <thawatchai.t@carpass.co.th>
2015-01-13 10:13:16 +0700

    Add -AddressMap -add method GetAll(start,limit) return List<AddressViewModel>
    Signed-off-by: Thawatchai-t <thawatchai.t@carpass.co.th>

--- KTBLeasing.Mapping/AddressMap.cs
+++ KTBLeasing.Mapping/AddressMap.cs
@@ -9,20 +9,22 @@ namespace KTBLeasing.FrontLeasing.Mapping.Orcl {
     
     public class AddressMap : ClassMap<Address> {
         
-        public AddressMap() {
+       public AddressMap() {
             Table("ADDRESS");
             LazyLoad();
-            Id(x => x.Id, "ID").GeneratedBy.Sequence("SEQ_ADDRESS");
-            References(x => x.Company).Column("COMPANY_ID");
+            Id(x => x.Id, "ID").GeneratedBy.Increment(); 
+            Map(x => x.CustomerId).Column("CUSTOMER_ID"); 
             Map(x => x.AddressTh).Column("ADDRESS_TH");
-            Map(x => x.AddressEng).Column("ADDRESS_ENG").Not.Nullable();
-            Map(x => x.Zipcode).Column("ZIPCODE");
+            Map(x => x.AddressEng).Column("ADDRESS_ENG");
             Map(x => x.SubdistrictId).Column("SUBDISTRICT_ID");
+            Map(x => x.Zipcode).Column("ZIPCODE"); 
+            Map(x => x.Remark).Column("REMARK");
+            Map(x => x.AddressType).Column("ADDRESS_TYPE");
+            Map(x => x.Active).Column("ACTIVE"); 
             Map(x => x.CreateDate).Column("CREATE_DATE");
             Map(x => x.UpdateDate).Column("UPDATE_DATE");
             Map(x => x.CreateBy).Column("CREATE_BY");
             Map(x => x.UpdateBy).Column("UPDATE_BY");
-            Map(x => x.Remark).Column("REMARK");
         }
         //old
         //public AddressMap() {
--- KTBLeasing.Mapping/Reposotory/AddressRepository.cs
+++ KTBLeasing.Mapping/Reposotory/AddressRepository.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using KTBLeasing.FrontLeasing.Domain;
+using KTBLeasing.Domain;
 
 namespace KTBLeasing.FrontLeasing.Mapping.Orcl.Reposotory
 {
-    class AddressRepository : NhRepository
+    public interface IAddressRepository
+    {
+        void Insert(Address entity);
+        List<Address> GetAll();
+        List<Address> GetAllWithOrderBy(string orderby);
+        /// <summary>
+        /// [20150113] Thawatchai.t 
+        /// Get Addresss view tab customer
+        ///
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        List<AddressViewModel> GetAll(int start, int limit);
+        int Count();
+        List<Address> Find(int start, int limit, string text);
+        int Count(string text);
+    }
+    public class AddressRepository : NhRepository, IAddressRepository
     {
         
         public void Insert(Address entity)
@@ -24,7 +43,8 @@ namespace KTBLeasing.FrontLeasing.Mapping.Orcl.Reposotory
         {
             using (var session = SessionFactory.OpenSession())
             {
-                //return session.QueryOver<Address>().List<Address>() as List<Address>;
+                return session.QueryOver<Address>().List<Address>() as List<Address>;
+                
                 return this.ExecuteICriteria<Address>() as List<Address>;
             }
         }
@@ -34,12 +54,51 @@ namespace KTBLeasing.FrontLeasing.Mapping.Orcl.Reposotory
             return this.ExecuteICriteriaOrderBy<Address>(new Address(), orderby) as List<Address>;
         }
 
-        public List<Address> GetAll(int start, int limit)
+        //public List<Address> GetAll(int start, int limit)
+        //{
+        //    using (var session = SessionFactory.OpenSession())
+        //    {
+        //        var result = session.QueryOver<Address>().Skip(start).Take(limit);
+        //        return result.List<Address>() as List<Address>;
+        //    }
+        //}
+        /// <summary>
+        /// [20150113] Thawatchai.t 
+        /// Get Addresss view tab customer
+        ///  
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="limit"></param>
+        /// <returns></returns>
+        public List<AddressViewModel> GetAll(int start, int limit)
         {
             using (var session = SessionFactory.OpenSession())
             {
-                var result = session.QueryOver<Address>().Skip(start).Take(limit);
-                return result.List<Address>() as List<Address>;
+                var result = (from x in session.QueryOver<Address>().List()
+                              join y in session.QueryOver<Province>().List()
+                              on x.SubdistrictId equals y.SubdistrictId
+                              select (new AddressViewModel
+                              {
+                                  AddressEng = x.AddressEng,
+                                  AddressTh = x.AddressTh,
+                                  CompanyId = x.CustomerId,
+                                  DistrictId = y.DistrictId,
+                                  DistrictName = y.DistrictName,
+                                  Id = x.Id,
+                                  ProvinceId = y.ProvinceId,
+                                  ProvinceName = y.ProvinceName,
+                                  Remark = x.Remark,
+                                  SubdistrictId = y.SubdistrictId,
+                                  SubdistrictName = y.SubdistrictName,
+                                  Zipcode = y.Zipcode,
+                                  DisplayProvince = string.Format("{0} {1} {2} {3}",y.ProvinceName,y.DistrictName,y.SubdistrictName,y.Zipcode)
+
+                              })
+                             ).ToList<AddressViewModel>();
+
+                return result as List<AddressViewModel>;
+
+                
             }
         }
 
