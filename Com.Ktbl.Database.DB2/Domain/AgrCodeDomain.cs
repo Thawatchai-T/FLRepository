fdd3d285b6601cc274912be7fa8f773c2b239afd
pomnot <pomnot@gmail.com>
2015-05-14 10:11:30 +0700

    Add DB2 Repository
    Signed-off-by: pomnot <pomnot@gmail.com>

---
+++ Com.Ktbl.Database.DB2/Domain/AgrCodeDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Com.Ktbl.Database.DB2.Domain
+{
+    public class AgrCodeDomain
+    {
+        public string ComId { get; set; }
+
+        public string AgrCode { get; set; }
+
+        public string CusCode { get; set; }
+    }
+}
