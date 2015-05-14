33470a9e4b3bcdca824b62b9a7f11b31f9a96282
Thawatchai-t <thawatchai.t@carpass.co.th>
2015-05-14 15:34:29 +0700

    Add missing file RoleInTabsMap.cs
    Signed-off-by: Thawatchai-t <thawatchai.t@carpass.co.th>

---
+++ KTBLeasing.Mapping/RoleInTabsMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KTBLeasing.Domain;
+using FluentNHibernate.Mapping;
+
+namespace KTBLeasing.FrontLeasing.Mapping.Orcl
+{
+    public class RoleInTabsMap : ClassMap<RoleInTabsDomain>
+    {
+       public RoleInTabsMap() {
+            Table("ROLE_IN_TABS");
+            LazyLoad();
+            CompositeId().KeyProperty(x => x.RoleId, "ROLE_ID")
+                         .KeyProperty(x => x.TabId, "TAB_ID");
+            References(x => x.Roles).Column("ROLE_ID");
+            References(x => x.TabManament).Column("TAB_ID");
+            Map(x => x.Remark).Column("REMARK").Length(150);
+            Map(x => x.Active).Column("ACTIVE").Length(22);
+       }
+    }
+}
