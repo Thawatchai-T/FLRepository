ab589006d6f96eafd377ea79d8cc63179a3c5fc0
Thawatchai-t <thawatchai.t@carpass.co.th>
2015-05-14 08:25:14 +0700

    commit code login

--- KTBLeasing.Helpers/Utilitys.cs
+++ KTBLeasing.Helpers/Utilitys.cs
@@ -19,6 +19,8 @@ namespace KTBLeasing.Helpers
     {
         public static string GenerateMD5(string input)
         {
+            string key = "pomnotwoody";
+            input = string.Format("{0,1}", key, input);
             MD5 md5Hasher = MD5.Create();
             byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
             return BitConverter.ToString(data);
