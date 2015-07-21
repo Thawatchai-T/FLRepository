9599e6269a1b92ba836f079efaab5f3693516bbe
pomnot <pomnot@gmail.com>
2015-07-21 10:18:24 +0700

    Add Job Master
    Signed-off-by: pomnot <pomnot@gmail.com>

---
+++ KTBLeasing.FrontLeasing/Controllers/JobController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using KTBLeasing.FrontLeasing.Models;
+using Newtonsoft.Json.Linq;
+using KTBLeasing.FrontLeasing.Domain;
+using log4net;
+using System.Reflection;
+using KTBLeasing.FrontLeasing.Mapping.Orcl.Reposotory;
+using Newtonsoft.Json;
+
+namespace KTBLeasing.FrontLeasing.Controllers
+{
+    public class JobController : ApiController
+    {
+        private IJobRepository jobRepository { get; set; }
+        private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        // GET api/job
+        public JobModel Get(int start, int limit)
+        {
+            try
+            {
+                JobModel model = new JobModel();
+                model.data = jobRepository.Get(start, limit);
+                model.total = jobRepository.Count();
+
+                return model;
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex);
+                return null;
+            }
+        }
+
+        // POST api/job
+        public void Post(Job entity)
+        {
+            jobRepository.SaveOrUpdate(entity);
+        }
+
+        // PUT api/job/5
+        public void Put(long id, Job entity)
+        {
+            jobRepository.Update(entity);
+        }
+
+        // DELETE api/job/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ KTBLeasing.FrontLeasing/Models/JobModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KTBLeasing.FrontLeasing.Domain;
+
+namespace KTBLeasing.FrontLeasing.Models
+{
+    public class JobModel
+    {
+        public List<Job> data { get; set; }
+        public int total { get; set; }
+    }
+}
